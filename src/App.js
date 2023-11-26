2f43ba84bb83b0df2562b16569dcc7c3354cc7ff
alexdancer <alexdancer05@gmail.com>
2023-11-26 15:09:16 -0600

    Added the useState for memory of when a box is clicked

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-function Square({ value }) {
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log("clicked!");
+    setValue('X');
   }
 
   return (
-    <button 
-      className="sqaure"
+    <button
+      className="square"
       onClick={handleClick}
-      >{value}</button>
+    >
+      {value}
+    </button>
   );
 }
- 
- export default function Board() {
-   return (
-   <>
-     <div class="board-row">
-       <Square value="1"/>
-       <Square value="2"/>
-       <Square value="3"/>
-     </div>
-     <div class="board-row">
-       <Square value="4"/>
-       <Square value="5"/>
-       <Square value="6"/>
-     </div>
-     <div class="board-row">
-       <Square value="7"/>
-       <Square value="8"/>
-       <Square value="9"/>
-     </div>
-   </>
-   );
- }
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
